77cdbc604000ee61b38390b6eea4babe034e13a9
johndev07 <alwynjohn.dev@gmail.com>
2023-02-07 16:53:45 +0530

    amazon front page

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232f3e",
+          DEFAULT: "#131921",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
